f580577fa2e2dcdc21f3ec9f13c520f3771ea87e
Viktor <viktor@apilab.ru>
2022-01-17 00:35:31 +0300

    frontend 1.4 update - add firebase and migrate new api version

--- frontend/environments/environment.ts
+++ frontend/environments/environment.ts
@@ -4,10 +4,20 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://filecabinet.enotolyb.ru/',
+  apiUrl: 'http://filecabinet.enotolyb.ru/', // 'http://filecabinet.enotolyb.ru/', // http://file-cab.local:3000/
   sentry: {
     dsn: 'https://d04d06eb22c14effb36c76b0453dea5c@o1087993.ingest.sentry.io/6103397',
     tracesSampleRate: 1.0,
+  },
+  firebase: {
+    apiKey: 'AIzaSyD5qwE1oJkYrJCzdcgkHn5iE0dclwO5ZtU',
+    authDomain: 'filecab-5454b.firebaseapp.com',
+    databaseURL: 'https://filecab-5454b-default-rtdb.firebaseio.com',
+    projectId: 'filecab-5454b',
+    storageBucket: 'filecab-5454b.appspot.com',
+    messagingSenderId: '167501688709',
+    appId: '1:167501688709:web:85e0020323cc518ae85eec',
+    measurementId: 'G-S2126YXX40',
   },
 };
 
